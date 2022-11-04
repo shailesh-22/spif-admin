1634e5f0f5fcac94ee8a39324f4e367317d818e3
Shahista <shahista@veniteck.com>
2022-11-04 17:42:08 +0530

    Second Commit

--- src/component/Registration/Register2.jsx
+++ src/component/Registration/Register2.jsx
@@ -1,10 +1,10 @@
-import { Typography, Card, Grid, TextField, Button } from '@material-ui/core'
+import { Typography, Card, Grid, TextField, Button, Input } from '@material-ui/core'
 import { CardContent } from '@mui/material'
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormLabel from '@mui/material/FormLabel';
-import React from 'react'
+import React, { useState } from 'react'
 import Stack from '@mui/material/Stack';
 import { useForm, Controller } from "react-hook-form"
 import MenuItem from '@mui/material/MenuItem';
@@ -12,9 +12,11 @@ import InputLabel from '@mui/material/InputLabel';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import FormControl from '@mui/material/FormControl';
 import { makeStyles } from '@material-ui/core/styles';
-
+import { useHistory } from "react-router-dom";
 import Header2 from '../Sidebar/Header2';
 import { ClassNames } from '@emotion/react';
+
+import { addUser } from './api'
 
 const useStyles = makeStyles((theme) => ({
   inputField: {
@@ -23,12 +25,41 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email : "",
+  phone: "",
+  gender: "",
+  date_of_birth: "",
+  profession :"",
+  street : "",
+  city:"",
+  state:"",
+  country: "",
+  pincode: ""
+}
+
 
 const Register2 = () => {
 
+  const history = useHistory()
 
-  const { inputregister, handleSubmit, control } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const [user, setUser] = useState(initialValues);
+
+  const onValueChange = (e) => {
+   
+    setUser({...user, [e.target.name]: e.target.value })
+    console.log(user);
+  }
+
+  const addUserDetails = async () => {
+   await addUser(user)
+   history.push("/assessment-test")
+  }
+
+  // const { inputregister, handleSubmit, control } = useForm();
+  // const onSubmit = (data) => console.log(data);
 
   const margin = { margin: "20px 5px" }
 
@@ -42,7 +73,7 @@ const Register2 = () => {
         <Card style={{ maxWidth: 1500, margin: "0 auto", padding: "0px 12px" }}>
           <CardContent>
             <Typography gutterbutton="true" variant="h5">Fill out your Basic details</Typography>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form >
 
               <Typography variant="body2" align="left" style={{ padding: "20px 0px" }} gutterbutton="true" >Personal info :</Typography>
 
@@ -57,8 +88,9 @@ const Register2 = () => {
                     fullWidth
                     required
                     name='firstName'
-                    inputRef={inputregister}
+                    onChange={(e) => onValueChange(e)}
                   />
+
                 </Grid>
 
                 {/* Textfield */}
@@ -70,7 +102,8 @@ const Register2 = () => {
                     fullWidth
                     required
                     name='lastName'
-                    inputRef={inputregister}
+                    onChange={(e) => onValueChange(e)}
+                  // inputRef={inputregister}
                   />
                 </Grid>
 
@@ -84,7 +117,8 @@ const Register2 = () => {
                     fullWidth
                     required
                     name='email'
-                    inputRef={inputregister}
+                    onChange={(e) => onValueChange(e)}
+                  // inputRef={inputregister}
                   />
                 </Grid>
 
@@ -98,7 +132,8 @@ const Register2 = () => {
                     fullWidth
                     required
                     name='phone'
-                    inputRef={inputregister}
+                    onChange={(e) => onValueChange(e)}
+                  // inputRef={inputregister}
                   />
                 </Grid>
 
@@ -108,58 +143,48 @@ const Register2 = () => {
                   <RadioGroup
                     row
                     aria-labelledby="demo-row-radio-buttons-group-label"
-                    name="row-radio-buttons-group"
+
+                    name="gender"
                   >
-                    <FormControlLabel value="female" control={<Radio inputRef={inputregister} />} label="Female" />
-                    <FormControlLabel value="male" control={<Radio inputRef={inputregister} />} label="Male" />
-                    <FormControlLabel value="other" control={<Radio inputRef={inputregister} />} label="Other" />
+                    <FormControlLabel value="female" control={<Radio onChange={(e) => onValueChange(e)} />} label="Female" />
+                    <FormControlLabel value="male" control={<Radio onChange={(e) => onValueChange(e)} />} label="Male" />
+                    <FormControlLabel value="other" control={<Radio onChange={(e) => onValueChange(e)} />} label="Other" />
                   </RadioGroup>
                 </Grid>
 
                 <Grid xs={12} sm={4} item>
                   <Stack component="form" noValidate spacing={3}>
-                    <Controller
-                    render={(props) =>(
-                      <TextField
+                    <TextField
                       id="date"
                       label="Date-of-Birth"
                       type="date"
                       defaultValue="2017-05-24"
-                      value={props.value}
-                      onChange={props.onChange}
                       sx={{ width: 220 }}
                       InputLabelProps={{
-                      shrink: true,
+                        shrink: true,
                       }}
+                      name="date_of_birth"
+                      onChange={(e) => onValueChange(e)}
                     />
-                    )}
-                    name="date_of_birth"
-                    control={control}
-                    />
-                    
                   </Stack>
                 </Grid>
 
 
                 <Grid xs={12} sm={4} item>
-                  <FormControl fullWidth variant="standard" className={ClassNames.inputField}>
-                    <InputLabel id="demo-simple-select-label">Profession</InputLabel>
-                    <Controller
-                      render={(props) => (
-                        <Select value={props.value} onChange={props.onChange}>
-                        {/* <MenuItem value="">Select your profession</MenuItem> */}
-                        <MenuItem value="Employed">Employed</MenuItem>
-                        <MenuItem value="Business Owners">Business Owners</MenuItem>
-                        <MenuItem value="Seeking Employement">Seeking Employement</MenuItem>
-                        <MenuItem value="Student">Student</MenuItem>
-                        <MenuItem value="Parent">Parent</MenuItem>
-                        <MenuItem value="Others">Others</MenuItem>
-                      </Select>
-                      )}
-                      name="profession"
-                      control={control}
-                      // defaultValue="ui/ux"
-                    />
+                  <FormControl fullWidth variant="standard" className={ClassNames.inputField} >
+                    <InputLabel id="demo-simple-select-label" >Profession</InputLabel>
+
+                    <Select name="profession"  onChange={(e) => onValueChange(e)}     >
+
+                      <MenuItem value="Employed" >Employed</MenuItem>
+                      <MenuItem value="Business Owners" >Business Owners</MenuItem>
+                      <MenuItem value="Seeking Employement" >Seeking Employement</MenuItem>
+                      <MenuItem value="Student"  >Student</MenuItem>
+                      <MenuItem value="Parent" >Parent</MenuItem>
+                      <MenuItem value="Others" >Others</MenuItem>
+
+                    </Select>
+                  
                   </FormControl>
                 </Grid>
 
@@ -170,19 +195,19 @@ const Register2 = () => {
               <Grid container spacing={4}>
 
                 <Grid xs={12} sm={4} item>
-                  <TextField label="Street" placeholder='Enter street' variant="standard" fullWidth required />
+                  <TextField label="Street" placeholder='Enter street' variant="standard" name="street" onChange={(e) => onValueChange(e)} fullWidth required />
                 </Grid>
                 <Grid xs={12} sm={4} item>
-                  <TextField label="City" placeholder='Enter city name' variant="standard" fullWidth required />
+                  <TextField label="City" placeholder='Enter city name' variant="standard" name="city" onChange={(e) => onValueChange(e)} fullWidth required />
                 </Grid>
                 <Grid xs={12} sm={4} item>
-                  <TextField label="State" placeholder='Enter State' variant="standard" fullWidth required />
+                  <TextField label="State" placeholder='Enter State' variant="standard" name="state" onChange={(e) => onValueChange(e)} fullWidth required />
                 </Grid>
                 <Grid xs={12} sm={4} item>
-                  <TextField label="Country" placeholder='Enter Country Name' variant="standard" fullWidth required />
+                  <TextField label="Country" placeholder='Enter Country Name' variant="standard" name="country" onChange={(e) => onValueChange(e)} fullWidth required />
                 </Grid>
                 <Grid xs={12} sm={4} item>
-                  <TextField label="Pincode" placeholder='Enter pincode number' variant="standard" fullWidth required />
+                  <TextField label="Pincode" placeholder='Enter pincode number' variant="standard" name="pincode" onChange={(e) => onValueChange(e)} fullWidth required />
                 </Grid>
 
 
@@ -190,7 +215,7 @@ const Register2 = () => {
                 <Grid xs={12} align="center" item>
 
                   <Button style={margin} type='reset' variant="outlined" color="primary"  >Reset</Button>
-                  <Button type='submit' variant="contained" color="primary" >Submit</Button>
+                  <Button type='submit' onClick={() => addUserDetails()} variant="contained" color="primary" >Submit</Button>
                 </Grid>
 
               </Grid>
