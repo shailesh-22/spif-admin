e6dea05d2f2ee35fb715f37de32feb6073ce0a12
shailesh-22 <shailesh@veniteck.com>
2022-11-16 17:52:39 +0530

    Changes has been made with tab file

--- src/component/pages/Assessment/Admin-Statement/Tabs/Tabs.jsx
+++ src/component/pages/Assessment/Admin-Statement/Tabs/Tabs.jsx
@@ -13,24 +13,15 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Switch, FormControlLabel } from "@material-ui/core";
-
-import { getUpdateQuestionApi , getQuestiondetails} from "../AdminStatement/api2"; 
-
-
+import { useHistory, useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 
 const Tabs = ({ questions }) => {
-<<<<<<< HEAD
 
- 
-  const udpateQuestions = async () => {
-   await getUpdateQuestionApi(questions);
-  }
+const history = useHistory();
 
-  let [currentTab, setCurrentTab] = useState("1");
-=======
     let [currentTab, setCurrentTab] = useState("1");
->>>>>>> a0c74ea10a442e3ccc7e660655379d9a96a870c7
 
     let [checked, setChecked] = useState(false);
 
@@ -54,11 +45,12 @@ const Tabs = ({ questions }) => {
         setTextField(addOption);
     };
 
-  const handleDelete = (i) => {
-    const delVal = [...textField];
-    delVal.splice(i, 1);
-    setTextField(delVal);
-  };
+
+    const handleDelete = (i) => {
+        const delVal = [...textField];
+        delVal.splice(i, 1);
+        setTextField(delVal)
+    }
 
     const handleChange = (onChangeValue, i) => {
         const inputData = [...textField];
@@ -66,20 +58,12 @@ const Tabs = ({ questions }) => {
         setTextField(inputData);
     };
 
-<<<<<<< HEAD
-  //   const handleDelete = (i) => {
-  //     const delVal = [...textField];
-  //     delVal.splice(i, 1);
-  //     setTextField(delVal);
-  //   };
-=======
 
     //   const handleDelete = (i) => {
     //     const delVal = [...textField];
     //     delVal.splice(i, 1);
     //     setTextField(delVal);
     //   };
->>>>>>> a0c74ea10a442e3ccc7e660655379d9a96a870c7
 
     const [list, setList] = useState(questions);
 
@@ -87,16 +71,10 @@ const Tabs = ({ questions }) => {
         const result = Array.from(list);
         const [removed] = result.splice(startIndex, 1);
 
-<<<<<<< HEAD
-    result.splice(endIndex, 0, removed);
-    return result;
-  };
-=======
         result.splice(endIndex, 0, removed);
 
         return result;
     };
->>>>>>> a0c74ea10a442e3ccc7e660655379d9a96a870c7
 
     const onEnd = (result) => {
         // console.log(result);
@@ -108,8 +86,35 @@ const Tabs = ({ questions }) => {
         setList(reorder(list, sourceIndex, destinationIndex));
     };
 
+
+
+    const { sStatementID } = useParams();
+
+   const handleSubmit = (e)=>{
+    
+    console.log("hello");
+    e.preventDefault();
+
+    const questionDetails = {  }
+     
+    fetch("http://localhost:3004/questions"+sStatementID ,{
+      methode:"PUT",
+      headers:{"content-type":"application/json"},
+      body:JSON.stringify(questionDetails)
+    })
+    .then((res)=>{
+      alert('saved successfuly.')
+      history.push('/dashboard');
+    })
+    .catch((err)=>{
+      console.log(err.message);
+    })
+   }
+
+
     return (
         <div className="tabTypeQuestion">
+          <form onSubmit={handleSubmit}>
             <div className="tab">
                 <DragDropContext onDragEnd={onEnd}>
                     <Droppable droppableId="12345" direction="horizontal">
@@ -162,225 +167,6 @@ const Tabs = ({ questions }) => {
                         </button>
                     )
                 } */}
-<<<<<<< HEAD
-      </div>
-      <div className="content">
-        {questions.map((tab, i) => (
-          <div key={i}>
-            {currentTab === `${tab.sStatementID}` && (
-              <div>
-                <div>
-                  <h5> Statement </h5>
-                  <TextareaAutosize
-                    maxRows={4}
-                    aria-label="maximum height"
-                    defaultValue={tab.sDescription}
-                    style={{
-                      width: "100%",
-                      height: "150px",
-                      padding: "10px",
-                      outline: "none",
-                      border: "1px solid rgba(55, 59, 59, 0.2)",
-                      borderRadius: "5px",
-                    }}
-                  />
-                </div>
-                <hr />
-                {tab.options.length !== 0 && (
-                  <div>
-                    <h5> Option Details </h5>
-                    <TableContainer component={Paper}>
-                      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                        <TableHead>
-                          <TableRow sx={{ fontSize: 40 }}>
-                            <TableCell
-                              align="center"
-                              sx={{ fontSize: 20, color: "#007A3E" }}
-                            >
-                              {" "}
-                              Options{" "}
-                            </TableCell>
-                            <TableCell
-                              align="center"
-                              sx={{ fontSize: 20, color: "#007A3E" }}
-                            >
-                              Correct(Yes/No)
-                            </TableCell>
-                            <TableCell
-                              align="center"
-                              sx={{ fontSize: 20, color: "#007A3E" }}
-                            >
-                              Prompt(Wrong Answer)
-                            </TableCell>
-                            <TableCell
-                              align="center"
-                              sx={{ fontSize: 20, color: "#007A3E" }}
-                            >
-                              Action
-                            </TableCell>
-                          </TableRow>
-                        </TableHead>
-                        <TableBody>
-                          {tab.options.map((option, i) => (
-                            <TableRow
-                              key={i}
-                              sx={{
-                                "&:last-child td, &:last-child th": {
-                                  border: 0,
-                                },
-                              }}
-                            >
-                              <TableCell component="th" scope="row">
-                                <TextareaAutosize
-                                  maxRows={4}
-                                  aria-label="maximum height"
-                                  defaultValue={option.text}                                   
-                                  onChange={handleChange}
-                                  style={{
-                                    width: "100%",
-                                    fontSize: 17,
-                                    padding: "10px",
-                                    outline: "none",
-                                    border: "1px solid rgba(55, 59, 59, 0.2)",
-                                    borderRadius: "5px",
-                                  }}
-                                />
-                              </TableCell>
-                              <TableCell align="center">
-                                <FormControlLabel
-                                  control={
-                                    <Switch
-                                      checked={option.isAnswer}
-                                      onChange={handleAnswer}
-                                      color="primary"
-                                    />
-                                  }
-                                />
-                              </TableCell>
-                              <TableCell align="center">
-                                <TextareaAutosize
-                                  maxRows={4}
-                                  aria-label="maximum height"
-                                  defaultValue={option.isPrompt}
-                                  style={{
-                                    width: "100%",
-                                    fontSize: 17,
-                                    padding: "10px",
-                                    outline: "none",
-                                    border: "1px solid rgba(55, 59, 59, 0.2)",
-                                    borderRadius: "5px",
-                                  }}
-                                />
-                              </TableCell>
-                              <TableCell align="center">
-                                <button
-                                  style={{
-                                    height: "50px",
-                                    width: "50px",
-                                    borderRadius: "10px",
-                                  }}
-                                  onClick={() => {
-                                    handleDelete(i);
-                                  }}
-                                >
-                                  <DeleteIcon />
-                                </button>
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                          {textField.map((data, i) => (
-                            <TableRow
-                              key={i}
-                              sx={{
-                                "&:last-child td, &:last-child th": {
-                                  border: 0,
-                                },
-                              }}
-                            >
-                              <TableCell component="th" scope="row">
-                                <TextareaAutosize
-                                  maxRows={4}
-                                  aria-label="maximum height"
-                                  defaultValue={data}
-                                  style={{
-                                    width: "100%",
-                                    fontSize: 17,
-                                    padding: "10px",
-                                    outline: "none",
-                                    border: "1px solid rgba(55, 59, 59, 0.2)",
-                                    borderRadius: "5px",
-                                  }}
-                                />
-                              </TableCell>
-                              <TableCell
-                                align="center"
-                                style={{ fontSize: 17 }}
-                              >
-                                <FormControlLabel
-                                  control={
-                                    <Switch
-                                      checked={checked}
-                                      onChange={handleAnswer}
-                                      color="primary"
-                                    />
-                                  }
-                                />
-                              </TableCell>
-                              <TableCell align="center">
-                                <TextareaAutosize
-                                  maxRows={4}
-                                  aria-label="maximum height"
-                                  defaultValue={data}
-                                  style={{
-                                    width: "100%",
-                                    fontSize: 17,
-                                    padding: "10px",
-                                    outline: "none",
-                                    border: "1px solid rgba(55, 59, 59, 0.2)",
-                                    borderRadius: "5px",
-                                  }}
-                                />
-                              </TableCell>
-                              <TableCell align="center">
-                                <button
-                                  style={{
-                                    height: "50px",
-                                    width: "50px",
-                                    borderRadius: "10px",
-                                  }}
-                                  onClick={() => {
-                                    handleDelete(i);
-                                  }}
-                                >
-                                  <DeleteIcon />
-                                </button>
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                        </TableBody>
-                        <div style={{ margin: " 20px " }}>
-                          <Button
-                            className=" btn btn-outline-success"
-                            style={{
-                              textAlign: "center",
-                              width: "auto",
-                              height: "40px",
-                            }}
-                            onClick={() => handleAdd()}
-                          >
-                            Add Option
-                          </Button>
-                        </div>
-                      </Table>
-                    </TableContainer>
-                  </div>
-                )}
-                <div className="tab-body-btns">
-                  <button className="btn btn-primary" onAuxClick={() => udpateQuestions()} >UPDATE</button>
-                  <button className="btn btn-primary">DELETE</button>
-                </div>
-              </div>
-=======
 
             </div>
             <div className="content">
@@ -396,6 +182,7 @@ const Tabs = ({ questions }) => {
                                             maxRows={4}
                                             aria-label="maximum height"
                                             defaultValue={tab.sDescription}
+                                            value={tab.sDescription}
                                             style={{ width: '100%', height: "150px", padding: "10px", outline: "none", border: "1px solid rgba(55, 59, 59, 0.2)", borderRadius: "5px" }}
                                         />
                                     </div>
@@ -433,7 +220,6 @@ const Tabs = ({ questions }) => {
                                                                         control={<Switch checked={option.isAnswer}
                                                                             onChange={handleAnswer}
                                                                             color='primary'
-
                                                                         />}
                                                                     />
                                                                 </TableCell>
@@ -505,17 +291,46 @@ const Tabs = ({ questions }) => {
                                         </div>
                                     }
                                     <div className="tab-body-btns">
-                                        <button className="btn btn-primary">UPDATE</button>
+                                        <button className="btn btn-primary" type="submit" >UPDATE</button>
                                         <button className="btn btn-primary">DELELTE</button>
                                     </div>
                                 </div>
                             }
                         </div>
->>>>>>> a0c74ea10a442e3ccc7e660655379d9a96a870c7
             )}
             </div>
+            </form>
         </div>
     );
 };
 
 export default Tabs;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
