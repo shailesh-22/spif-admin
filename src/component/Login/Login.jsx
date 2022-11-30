b16ae5ec493668600012ae3f3eaa635b3c9bbbdb
shailesh-22 <shailesh@veniteck.com>
2022-11-30 10:43:39 +0530

    Changed Login url code.

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const url = "https://wheel.limeneal.com:8020/accounts/auth/";
+            const url = "http://103.160.153.38:8020/accounts/auth/";
             
             const { data: res } = await axios.post(url, data);
             localStorage.setItem("token", JSON.stringify(res.data));
